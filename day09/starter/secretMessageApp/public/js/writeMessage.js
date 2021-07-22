57382f01324c59ef52673bb2ebfc7ae211cdc2ee
Casey Avila <casey@theavilas.org>
2021-07-22 22:54:15 +0000

    Add independent work

--- day09/starter/secretMessageApp/public/js/writeMessage.js
+++ day09/starter/secretMessageApp/public/js/writeMessage.js
@@ -1,7 +1,9 @@
+const passcodeInput = document.querySelector("#passcode");
+
 const submitMessage = () => {
     console.log("Submitting message...");
 
-    const passcodeInput = document.querySelector("#passcode");
+    passcodeInput = document.querySelector("#passcode");
     const messageInput = document.querySelector("#message");
 
     const passcodeValue = passcodeInput.value;
@@ -16,4 +18,28 @@ const submitMessage = () => {
     passcodeInput.value = "";
     messageInput.value = "";
 };
+
+const notification = document.querySelector("#notification");
+const button = document.querySelector("#button");
+passcodeInput.addEventListener("input", e => {
+    let valid = false;
+
+    for (let i = 0; i < passcodeInput.value.length; i++) {
+        if ((passcodeInput.value.charCodeAt(i) <= 90 && passcodeInput.value.charCodeAt(i) >= 65) || (passcodeInput.value.charCodeAt(i) >= 48 && passcodeInput.value.charCodeAt(i) <= 57)) {
+            valid = true;
+            break;
+        }
+    }
+
+    if (!valid) {
+        button.style.pointerEvents = "none";
+        notification.innerHTML = "Please use at least one number or one capital letter";
+    } else {
+        button.style.pointerEvents = "auto";
+        notification.innerHTML = "Nice passcode";
+    }
+
+    
+});
+
 
